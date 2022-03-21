bdf2568912193eac6b37cfe5fc3b032cc99164e0
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2022-03-21 16:10:21 +0300

    support diem addresses without 0x prefix

--- pontem/src/lib.rs
+++ pontem/src/lib.rs
@@ -5,7 +5,18 @@ use anyhow::{Context, Result};
 use move_core_types::account_address::AccountAddress;
 
 pub fn parse_address(addr: &str) -> Result<AccountAddress> {
-    if addr.starts_with("0x") {
+    if let Ok(address) = ss58_to_address(addr) {
+        // first try ss58 parsing
+        Ok(address)
+    } else if cfg!(feature = "bech32_addr") && addr.starts_with(HRP) {
+        // try bech32 address
+        bech32_into_address(addr)
+    } else {
+        let mut addr = addr.to_string();
+        if !addr.starts_with("0x") {
+            addr = format!("0x{}", addr);
+        }
+        // try parsing hex diem/aptos address with optional 0x prefix
         let max_hex_len = AccountAddress::LENGTH * 2 + 2;
         if addr.len() > max_hex_len {
             return Err(anyhow::anyhow!(
@@ -14,12 +25,7 @@ pub fn parse_address(addr: &str) -> Result<AccountAddress> {
                 addr
             ));
         }
-
-        AccountAddress::from_hex_literal(addr).map_err(|err| err.into())
-    } else if cfg!(feature = "bech32_addr") && addr.starts_with(HRP) {
-        bech32_into_address(addr)
-    } else {
-        ss58_to_address(addr)
+        AccountAddress::from_hex_literal(&addr)
             .with_context(|| format!("Address {:?} is not a valid diem/pont address", addr))
     }
 }
