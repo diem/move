077093724880bcc566364b12476ff17aa817d40f
Teng Zhang <tengz@fb.com>
2022-06-07 14:22:25 -0700

    abi-read

---
+++ language/evm/move-ethereum-abi/src/abi_move_type.rs
@@ -0,0 +1,19 @@
+// Copyright (c) The Diem Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+
+use crate::abi_signature_type::ABIJsonSignature;
+
+/// Key for metadata
+const _ABI_ETHER_MOVE_KEY: &str = "abi_ethereum_move";
+
+#[derive(Serialize, Deserialize)]
+pub struct ABIMoveSignature {
+    // Move type -> Ethereum event abi
+    pub event_map: BTreeMap<String, ABIJsonSignature>,
+
+    // Move function -> Ethereum pub function abi
+    pub func_map: BTreeMap<String, ABIJsonSignature>,
+}
---
+++ language/evm/move-ethereum-abi/src/lib.rs
@@ -0,0 +1,7 @@
+// Copyright (c) The Diem Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+#![forbid(unsafe_code)]
+
+pub mod abi_move_type;
+pub mod abi_signature_type;
