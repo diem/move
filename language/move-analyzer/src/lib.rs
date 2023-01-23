47784bd30754bce88defdbaf8980482f4582baf1
satyamacn <satyam.arya@accenture.com>
2023-01-23 11:14:15 +0000

    [language server] Partial implementation of go-to-def and go-to-references
    Closes: #365

--- language/move-analyzer/src/lib.rs
+++ language/move-analyzer/src/lib.rs
@@ -1,6 +1,10 @@
 // Copyright (c) The Diem Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
+#[macro_use(sp)]
+extern crate move_ir_types;
+
 pub mod completion;
 pub mod context;
+pub mod symbols;
 pub mod vfs;
